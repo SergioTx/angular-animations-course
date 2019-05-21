c97213409f0dd3872c27866ad143c133ad652b91
SergioTx <txitxo37@gmail.com>
2019-05-21 12:30:36 +0200

    26

--- 1-dynamic-styles/src/app/author/author.component.ts
+++ 1-dynamic-styles/src/app/author/author.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   h1 {
     margin:0;
     font-size: 12px;
-    color: #8c8c8c;
+    // color: #8c8c8c;
   }
   `],
   encapsulation: ViewEncapsulation.Emulated,
