2c49ec6ce2f5fb9e4c3d459fd7d7f8efb5535019
SergioTx <txitxo37@gmail.com>
2019-05-22 13:01:00 +0200

    78

--- 2-prj-styling-start/src/app/projects/animations.ts
+++ 2-prj-styling-start/src/app/projects/animations.ts
@@ -1,5 +1,12 @@
-import { trigger } from "@angular/animations";
+import { trigger, state, style } from "@angular/animations";
 
 export const markedTrigger = trigger('markedTrigger', [
-
+  state('default', style({
+    border: '1px solid black',
+    backgroundColor: 'transparent',
+  })),
+  state('marked', style({
+    border: '2px solid blue',
+    backgroundColor: '#caeff9',
+  })),
 ]);
