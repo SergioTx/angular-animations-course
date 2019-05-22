4ee65e181221754a12cf56f7f228bad4406d6828
SergioTx <txitxo37@gmail.com>
2019-05-22 15:09:16 +0200

    86

--- 5-advanced-animations/src/app/animations.ts
+++ 5-advanced-animations/src/app/animations.ts
@@ -1,16 +1,22 @@
 import { state, trigger, style, transition, animate } from '@angular/animations';
 
 export const showStateTrigger = trigger('showState', [
-  state('shown', style({
-
-  })),
-  transition('void => shown', [
+  // transition('void => *', [
+  //   style({
+  //     opacity: 0,
+  //   }),
+  //   animate(300),
+  // ]),
+  // transition('* => void', animate(300, style({
+  //   opacity: 0,
+  // }))),
+  transition(':enter', [
     style({
       opacity: 0,
     }),
     animate(300),
   ]),
-  transition('shown => void', animate(300, style({
+  transition(':leave', animate(300, style({
     opacity: 0,
   }))),
 ]);
