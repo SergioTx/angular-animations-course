0fc761d10d076b032ed6945b09d16dbcbaedcb44
SergioTx <txitxo37@gmail.com>
2019-05-22 13:08:06 +0200

    80

--- 2-prj-styling-start/src/app/projects/animations.ts
+++ 2-prj-styling-start/src/app/projects/animations.ts
@@ -4,10 +4,25 @@ export const markedTrigger = trigger('markedTrigger', [
   state('default', style({
     border: '1px solid black',
     backgroundColor: 'transparent',
+    padding: '20px',
   })),
   state('marked', style({
     border: '2px solid blue',
     backgroundColor: '#caeff9',
+    padding: '19px',
   })),
-  transition('default <=> marked', animate('300ms ease-out')),
+  transition('default => marked', [
+    style({
+      border: '2px solid black',
+      padding: '19px',
+    }),
+    animate('300ms ease-out'),
+  ]),
+  transition('marked => default', [
+    style({
+      border: '1px solid blue',
+      padding: '20px',
+    }),
+    animate('300ms ease-out'),
+  ]),
 ]);
