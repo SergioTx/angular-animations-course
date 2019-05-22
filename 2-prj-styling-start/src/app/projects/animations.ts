5c043841534d282271f5101fc684a035a7c1309f
SergioTx <txitxo37@gmail.com>
2019-05-22 13:14:49 +0200

    82

--- 2-prj-styling-start/src/app/projects/animations.ts
+++ 2-prj-styling-start/src/app/projects/animations.ts
@@ -16,7 +16,12 @@ export const markedTrigger = trigger('markedTrigger', [
       border: '2px solid black',
       padding: '19px',
     }),
-    animate('300ms ease-out'),
+    animate('200ms ease-out',
+      style({
+        transform: 'scale(1.05)'
+      })
+    ),
+    animate(200),
   ]),
   transition('marked => default', [
     style({
