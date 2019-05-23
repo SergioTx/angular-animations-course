66ac0aca43dfc7d0dde75f7c410611545a151e9b
SergioTx <txitxo37@gmail.com>
2019-05-23 13:00:10 +0200

    116

--- 2-prj-styling-start/src/app/projects/animations.ts
+++ 2-prj-styling-start/src/app/projects/animations.ts
@@ -1,4 +1,4 @@
-import { trigger, state, style, transition, animate, keyframes } from "@angular/animations";
+import { trigger, state, style, transition, animate, keyframes, query, stagger } from "@angular/animations";
 
 export const markedTrigger = trigger('markedTrigger', [
   state('default', style({
@@ -33,29 +33,6 @@ export const markedTrigger = trigger('markedTrigger', [
 ]);
 
 export const itemStateTrigger = trigger('itemState', [
-  transition(':enter', [
-    style({
-      opacity: 0,
-      transform: 'translateX(-100%)',
-    }),
-    animate('500ms ease-out', keyframes([
-      style({
-        opacity: 0,
-        transform: 'translateX(-100%)',
-        offset: 0,
-      }),
-      style({
-        opacity: 1,
-        transform: 'translateX(15%)',
-        offset: 0.4,
-      }),
-      style({
-        opacity: 1,
-        transform: 'translateX(0)',
-        offset: 1,
-      }),
-    ]))
-  ]),
   transition(':leave', [
     animate('500ms ease-in', keyframes([
       style({
@@ -107,4 +84,29 @@ export const slideStateTrigger = trigger('slideState', [
       transform: 'translateY(-100%)',
     }))
   ]),
+]);
+
+export const listStateTrigger = trigger('listState', [
+  transition('*=>*', [
+    query(':enter', [
+      style({
+        opacity: 0,
+        transform: 'translateX(-100%)',
+      }),
+      stagger(300, [
+        animate('500ms ease-out', keyframes([
+          style({
+            opacity: 1,
+            transform: 'translateX(15%)',
+            offset: 0.4,
+          }),
+          style({
+            opacity: 1,
+            transform: 'translateX(0)',
+            offset: 1,
+          }),
+        ]))
+      ]),
+    ], { optional: true }),
+  ])
 ]);
--- 2-prj-styling-start/src/app/projects/projects.component.ts
+++ 2-prj-styling-start/src/app/projects/projects.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { Project } from './project.model';
 import { ProjectsService } from './projects.service';
-import { markedTrigger, itemStateTrigger, slideStateTrigger } from './animations';
+import { markedTrigger, itemStateTrigger, slideStateTrigger, listStateTrigger } from './animations';
 import { AnimationEvent } from '@angular/animations';
 import { routeFadeStateTrigger, routeSlideStateTrigger } from '../shared/route-animations';
 
@@ -15,13 +15,13 @@ import { routeFadeStateTrigger, routeSlideStateTrigger } from '../shared/route-a
     slideStateTrigger,
     routeFadeStateTrigger,
     routeSlideStateTrigger,
+    listStateTrigger,
   ],
 })
 export class ProjectsComponent implements OnInit {
   // @HostBinding('@routeFadeState') routeAnimation = true;
   @HostBinding('@routeSlideState') routeAnimation = true;
   projects: Project[];
-  displayedProjects: Project[] = [];
   markedPrjIndex = 0;
   progress = 'progressing';
   createNew = false;
@@ -34,9 +34,6 @@ export class ProjectsComponent implements OnInit {
         (prj: Project[]) => {
           this.progress = 'finished';
           this.projects = prj;
-          if (this.projects.length >= 1) {
-            this.displayedProjects.push(this.projects[0]);
-          }
         }
       );
   }
@@ -54,16 +51,5 @@ export class ProjectsComponent implements OnInit {
     setTimeout(() => {
       this.projects.unshift(project);
     }, 300);
-  }
-
-  onItemAnimated(animationEvent: AnimationEvent, lastPrjId: number) {
-    if (animationEvent.fromState != 'void') {
-      return;
-    }
-    if (this.projects.length > lastPrjId + 1) {
-      this.displayedProjects.push(this.projects[lastPrjId + 1]);
-    } else {
-      this.projects = this.displayedProjects;
-    }
   }
 }
